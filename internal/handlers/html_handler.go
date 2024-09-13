d1eed29e416ba4e7ec3c365da324c4abd63f3730
Vlad Bormisov <bormisov1@gmail.com>
2024-09-14 02:07:58 +0500

    fix(html_handler): format indentation

--- internal/handlers/html_handler.go
+++ internal/handlers/html_handler.go
@@ -1,19 +1,18 @@
-// internal/handlers/html_handler.go
 package handlers
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+	"net/http"
 )
 
 type HTMLHandler struct{}
 
 func (h *HTMLHandler) ServeMiniApp(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("templates/miniapp.html")
-    if err != nil {
-        http.Error(w, "Failed to load mini app", http.StatusInternalServerError)
-        return
-    }
+	tmpl, err := template.ParseFiles("templates/miniapp.html")
+	if err != nil {
+		http.Error(w, "Failed to load mini app", http.StatusInternalServerError)
+		return
+	}
 
-    tmpl.Execute(w, nil)
+	tmpl.Execute(w, nil)
 }
